7e95ee3344a8a0fe0511c9fad56966494bb01dc3
ViktorLohutenkov <viktor.lohutenkov@gmail.com>
2023-04-16 17:16:10 +0300

    .

---
+++ src/main/java/org/goit/Dto.java
@@ -0,0 +1,15 @@
+package org.goit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Dto {
+    private String name;
+    private String lastName;
+
+}
+
